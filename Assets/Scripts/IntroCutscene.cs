e5c497346fd7e717b8182c9a4d08d3a56cf6955c
Mentor <mentor@palokaj.co>
2015-12-13 13:52:19 +0100

    Empty intro cutscene

---
+++ Assets/Scripts/IntroCutscene.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroCutscene : MonoBehaviour {
+
+	MovieTexture movie;
+
+    // Use this for initialization
+	void Start () {
+		movie = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+		GetComponent<AudioSource>().clip = movie.audioClip;
+		GetComponent<AudioSource>().Play();
+		movie.Play();
+	}
+
+    // Update is called once per frame
+	void Update () {
+		if (!movie.isPlaying) {
+			Debug.Log("Movie done");
+			Application.LoadLevel(1);
+		}
+	}
+
+	void OnMouseUpAsButton() {
+		movie.Stop();
+	}
+}
