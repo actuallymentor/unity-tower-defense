2189c1c3b1e2c20fa759a80d8ba6dee19b1beeee
Mentor <mentor@palokaj.co>
2015-12-14 13:02:44 +0100

    Dalek bulb colors

---
+++ Assets/Scripts/BlinkYellow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinkYellow : MonoBehaviour {
+	public Color startcolor;
+
+	// Use this for initialization
+	void Start () {
+		startcolor = GetComponent<Renderer>().material.color;
+		GetComponent<Renderer>().material.color = Color.white;
+		InvokeRepeating("RedBlink", 1, 0.5f);
+		InvokeRepeating("YellowBlink", 1.5f, 0.5f);
+	}
+
+	void RedBlink() {
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+	
+	void YellowBlink() {
+		GetComponent<Renderer>().material.color = Color.yellow;
+	}
+}
