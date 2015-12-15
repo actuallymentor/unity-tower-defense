6592ae82e50848ff82ec0299be9eb4d35f4a5117
Mentor <mentor@palokaj.co>
2015-12-15 11:30:48 +0100

    Camera shake upon Dalek impact

--- Assets/Scripts/dalekDamage.cs
+++ Assets/Scripts/dalekDamage.cs
@@ -3,10 +3,15 @@ using System.Collections;
 
 public class dalekDamage : MonoBehaviour {
 	public variableControl varCont;
+	AudioSource _explosion;
+	public ShakeCamera _cam;
 
 	void Start() {
 		GameObject theGround = GameObject.Find("Ground");
+		GameObject _theCamera = GameObject.Find("Main Camera");
 		varCont = theGround.GetComponent<variableControl>();
+		_cam = _theCamera.GetComponent<ShakeCamera>();
+		_explosion = GetComponent<AudioSource>();
 	}
 
 	void Update() {
@@ -21,7 +26,13 @@ public class dalekDamage : MonoBehaviour {
 		if (other.gameObject.name == "ActualDalek(Clone)") {
 			varCont.tardisDecrease(varCont.enemyDifficulty);
 			Debug.Log ("Current health is " + varCont.tardisCurrent() );
+			_explosion.Play();
+			_cam.Shake();
 			Destroy (other.gameObject);
 		}
 	}
+
+	// Camera shake
+
+
 }
