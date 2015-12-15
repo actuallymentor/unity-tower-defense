2a3236553941954b61b8b4543a4a416ca1d1c663
Mentor <mentor@palokaj.co>
2015-12-15 07:19:32 +0100

    Random exterminate pitch and frequency at spawn

---
+++ Assets/Scripts/SayExterminate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SayExterminate : MonoBehaviour {
+	AudioSource _exterminate;
+	public float _speakingFrequency;
+
+	// Use this for initialization
+	void Start () {
+		_exterminate = GetComponent<AudioSource>();
+		_exterminate.pitch += Random.Range(-0.1F, 0.8F);
+		_speakingFrequency = Random.Range(2F, 5F);
+		_exterminate.PlayDelayed(_speakingFrequency);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
